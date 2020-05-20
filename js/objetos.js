b53def2898a092986264cf20fd2f7de4d93b986f
gboned <gboned@cifpfbmoll.eu>
2020-05-20 11:26:48 +0200

    Crear método para promocionar de curso al alumno

--- js/objetos.js
+++ js/objetos.js
@@ -12,5 +12,20 @@ var alumno = {
     segundoApellido: 'Sanz',
     nivel: 'ESO',
     curso: 'Primero',
-    grupo: 'A'
+    grupo: 'A',
+  
+    // Creo un método para que devuelva la información principal del alumno.
+    // Para llamar a esta función se hace escribiendo alumno.fichaDeAlumno().
+    fichaDeAlumno: function() {
+        console.log(`DNI: ${this.dni}. Nombre: ${this.nombre}. Apellidos: ${this.primerApellido} ${this.segundoApellido}.`);
+
+    },
+
+    // Creo un método que indique que el alumno ha pasado de curso.
+    // Para llamar a esta función se hace escribiendo alumno.promocionarCurso().
+    promocionarCurso: function() {
+        this.curso = "Segundo";
+        console.log(`El alumno con DNI ${this.dni} ha promocionado al curso ${this.curso} de la ${this.nivel}.`)
+
+    }
 };
