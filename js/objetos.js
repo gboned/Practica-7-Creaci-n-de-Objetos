fc605ac542bf749f081060a760f90cda1180a10f
gboned <gboned@cifpfbmoll.eu>
2020-05-20 10:59:36 +0200

    Crear primer objeto

--- js/objetos.js
+++ js/objetos.js
@@ -4,3 +4,13 @@
 /*                            */
 /******************************/
 
+// Creo el primer 'objeto', que será un alumno.
+var alumno = {
+    dni: '47410562P',
+    nombre: 'Daniel',
+    primerApellido: 'Ruiz',
+    segundoApellido: 'Sanz',
+    nivel: 'ESO',
+    curso: 'Primero',
+    grupo: 'A'
+};
